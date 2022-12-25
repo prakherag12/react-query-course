24f1d75e456c1e3415ea2c9543e301ed97756d55
prakhar <prakherag12@gmail.com>
2022-12-25 13:56:00 +0530

    issues List

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,35 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { IssueItem } from "./IssueItem";
 
 export default function IssuesList() {
+  const { data, isLoading } = useQuery(["issues"], () =>
+    fetch("/api/issues").then((rs) => rs.json())
+  );
+
+  console.log(data);
+
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <ul className="issues-list">
+          {data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentCount={issue.comments.length}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              labels={issue.labels}
+              status={issue.status}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
